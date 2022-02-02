dccbe11668d82f7606a277f1dac7423bc5a3bbd7
reload1bronze <nature9th@gmail.com>
2022-02-02 21:46:40 +0900

    #8.8 Netflix Clone 1. Home page

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
-import { darkTheme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { theme } from "./theme";
 
 import { createGlobalStyle } from "styled-components";
 
@@ -72,8 +73,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    color: black;
+    color: ${(props) => props.theme.white.darker};
     line-height: 1.2;
+    background-color: black;
   }
 
   a {
@@ -82,13 +84,17 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
